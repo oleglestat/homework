ca721e701962d4c9765cf0052e5756ae1b1cb768
Oleg Chmut <oleg@boylesoftware.com>
2023-10-17 14:17:06 +0300

    homework3.js - done

---
+++ homework3.js
@@ -0,0 +1,104 @@
+/*
+  1. Напишіть функцію addThemAll
+ */
+console.log(addThemAll(2,4)); // 6
+console.log(addThemAll(1,2,3,4)); // 10
+console.log(addThemAll(5,5,10)); // 20
+
+function addThemAll(...args) {
+	return args.reduce( (accumulator, currentValue ) => accumulator + currentValue );
+}
+
+/*
+  2. Задача на використання замикання.
+ */
+console.log(multiply(5)(5))		// 25
+console.log(multiply(2)(-2))	        // -4
+console.log(multiply(4)(3))		// 12
+
+function multiply(a) {
+	return (b) => a*b;
+}
+
+/*
+  3. Напишіть функцію яка буде використовуватись для сортування масиву фільмів
+ */
+
+const movies = [
+	{
+		movieName: 'The Thing',
+		releaseYear: 1982,
+		directedBy: 'Carpenter',
+		runningTimeInMinutes: 109,
+	},
+	{
+		movieName: 'Aliens',
+		releaseYear: 1986,
+		directedBy: 'Cameron',
+		runningTimeInMinutes: 137,
+	},
+	{
+		movieName: 'Men in Black',
+		releaseYear: 1997,
+		directedBy: 'Sonnenfeld',
+		runningTimeInMinutes: 98,
+	},
+	{
+		movieName: 'Predator',
+		releaseYear: 1987,
+		directedBy: 'McTiernan',
+		runningTimeInMinutes: 107,
+	},
+];
+
+console.log(movies.sort(byProperty('releaseYear', '>')));
+// // виведе масив фільмів посортованих по року випуску, від старішого до новішого
+console.log(movies.sort(byProperty('runningTimeInMinutes', '<')));
+// // виведе масив фільмів посортованих по їх тривалості, від найдовшого до найкоротшого
+console.log(movies.sort(byProperty('movieName', '>')));
+// виведе масив фільмів посортованих по назві, в алфавітному порядку
+
+function byProperty(property, direction) {
+	return function ( a, b ) {
+		if( typeof a[property] === 'number') {
+			if( direction === '>') {
+				return a[property] - b[property];
+			} else if( direction === '<' ) {
+				return b[property] - a[property];
+			}
+		} else if( typeof a[property] === 'string' ) {
+			let nameA = a[property];
+			let nameB = b[property];
+			if( direction === '>') {
+				if( nameA > nameB ) {
+					return 1;
+				}
+				if( nameA < nameB ) {
+					return -1;
+				}
+				return 0
+			} else if( direction === '<' ) {
+				if( nameA > nameB ) {
+					return -1;
+				}
+				if( nameA < nameB ) {
+					return 1;
+				}
+				return 0
+			}
+		}
+	}
+}
+
+
+/*
+  4. Напишіть функцію яка відфільтрує масив унікальних значень
+ */
+
+const userNames = ['Петро', 'Емма', 'Петро', 'Емма', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена', 'Емма'];
+
+function filterUnique(array) {
+  return Array.from(new Set(array));
+}
+
+console.log(filterUnique(userNames)); // ['Петро', 'Емма', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена'];
