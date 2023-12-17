7a61d5d329422deea26ba28201cd64bb401b6474
Oleg Chmut <oleg@boylesoftware.com>
2023-12-17 19:09:46 +0200

    course - in progress

--- course_project/calculator.js
+++ course_project/calculator.js
@@ -1,8 +1,60 @@
 "use strict";
 import { token } from "./constants.js";
+let form = document.forms[ 'calculator' ];
+let dateInput= document.querySelector( '#date-one' );
+let dateSecondInput= document.querySelector( '#date-two' );
+let dayButton= document.querySelector( '#day' );
+let monthButton= document.querySelector( '#month' );
 
-(function (){
 
-})();
+
+(() => {
+	form.addEventListener( 'submit', function ( event ) {
+		event.preventDefault();
+
+		console.log(
+			this.dateOne.value,
+			this.dateTwo.value,
+			this.days.value,
+			this.time.value
+		);
+		if( this.days.value === 'all' && this.time.value === 'days') {
+			console.log( (this.dateTwo.valueAsNumber - this.dateOne.valueAsNumber)/ (24 * 60 * 60 * 1000) );
+		}
+		const start = new Date( this.dateOne.value );
+		const end = new Date( this.dateTwo.value );
+		let loop = new Date(start);
+		while (loop <= end) {
+			let newDate = loop.setDate(loop.getDate() + 1);
+			loop = new Date(newDate);
+		}
+
+		form.reset();
+	});
+
+	dateInput.oninput = function() {
+		if( this.value ) {
+			dateSecondInput.removeAttribute( 'disabled' );
+			dayButton.removeAttribute( 'disabled' );
+			monthButton.removeAttribute( 'disabled' );
+		} else {
+			dateSecondInput.setAttribute('disabled', '' );
+			dayButton.setAttribute('disabled', '' );
+			monthButton.setAttribute('disabled', '' );
+		}
+	};
+
+	dayButton.onclick = function( event ) {
+		event.preventDefault();
+		dateSecondInput.valueAsDate = new Date( dateInput.valueAsNumber + 7 * 24 * 60 * 60 * 1000 );
+	}
+
+	monthButton.onclick = function( event ) {
+		event.preventDefault();
+		dateSecondInput.valueAsDate = new Date( dateInput.valueAsNumber + 30 * 24 * 60 * 60 * 1000 );
+	}
+} )();
+
+
 
 
